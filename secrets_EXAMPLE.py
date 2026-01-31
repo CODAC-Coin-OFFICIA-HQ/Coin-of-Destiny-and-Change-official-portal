5ea9be83f5050ce743746427afb8f043b52371c6
Your GitHub Name <your-email@example.com>
2026-01-31 19:05:05 +0800

    Init: Project Security & References

---
+++ secrets_EXAMPLE.py
@@ -0,0 +1,7 @@
+# --- CODAC TEMPLATE ---
+# Rename this file to 'secrets.py' and enter your keys below.
+# DO NOT UPLOAD YOUR REAL KEYS.
+
+DB_PASSWORD = "ENTER_PASSWORD_HERE"
+API_KEY = "ENTER_KEY_HERE"
+FOUNDER_KEY = "ENTER_PRIVATE_KEY_HERE"
